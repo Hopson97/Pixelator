1ebee2baec5bd094846a957253f6c450b70a62b5
Hopson97 <mhopson@hotmail.co.uk>
2018-10-05 21:35:28 +0100

    begin work on validation

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 
-int main(int argc, char** argv) {
+bool isValidSize(unsigned size, unsigned pxlSize) {
     
+}
+
+int main(int argc, char** argv) {
+    std::string fileName;
+    int pixelSize = 0;
+    if (argc > 2) {
+        std::string fileName = argv[1];
+        std::string pxString = argv[2];
+        pixelSize = std::stoi(pxString); //TODO Error check
+    }
+    else {
+        std::cout << "Usage: \n";
+        std::cout << "pixelator <image-file-path> <pixel-size>";
+        return 0;
+    }
+
+    std::cout << "Loading image...\n";
+    sf::Image image;
+    image.loadFromFile(fileName);
+
+    std::cout << "Validating image size to pixel size ratio...\n";    
+    if (image.getSize().x % pixelSize != 0) {
+        std::cout << "ERROR: Image width is not divisible by pixel size.\n";
+        return -1;
+    }
+    if (image.getSize().x % pixelSize != 0) {
+        std::cout << "ERROR: Image height is not divisible by pixel size.\n";
+        return -1;
+    }
 }
